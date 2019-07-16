49eb18c34c7fa84e9acf1d3af43240f0ad30c271
Nguyen Van Duc <vanduc.dn@gmail.com>
2019-07-16 22:14:10 +0700

    Stage update

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -2,25 +2,33 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 table="",
+                 s3_bucket="",
+                 s3_key="",
+                 region="",
+                 file_format="JSON",
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.region = region
+        self.file_format = file_format
+        self.aws_credentials_id = aws_credentials_id
+        self.execution_date = kwargs.get('execution_date')
 
     def execute(self, context):
         self.log.info('StageToRedshiftOperator not implemented yet')
-
-
-
-
-
