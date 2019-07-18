173c22573cadebc3d9c48f83ba752162084290cd
Nguyen Van Duc <vanduc.dn@gmail.com>
2019-07-18 20:37:24 +0700

    Update staging operator

--- dags/dag.py
+++ dags/dag.py
@@ -3,7 +3,7 @@ import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
-                               LoadDimensionOperator, DataQualityOperator)
+                               LoadDimensionOperator, DataQualityOperator, CreateTablesOperator)
 from helpers import SqlQueries
 
 # AWS_KEY = os.environ.get('AWS_KEY')
--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,6 +1,7 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
 
 
 class StageToRedshiftOperator(BaseOperator):
@@ -18,9 +19,6 @@ class StageToRedshiftOperator(BaseOperator):
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
         self.table = table
         self.redshift_conn_id = redshift_conn_id
         self.s3_bucket = s3_bucket
@@ -31,4 +29,26 @@ class StageToRedshiftOperator(BaseOperator):
         self.execution_date = kwargs.get('execution_date')
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        redshift.run(f"DELETE FROM {self.table}")
+
+        s3_path = f"s3://{self.s3_bucket}"
+        if self.execution_date:
+            year = self.execution_date.strftime("%Y")
+            month = self.execution_date.strftime("%m")
+            day = self.execution_date.strftime("%d")
+            s3_path = "/".join([s3_path, str(year), str(month), str(day)])
+        s3_path += "/" + self.s3_key
+
+        redshift.run(f"""
+            COPY {self.table} FROM '{s3_path}'
+            ACCESS_KEY_ID '{credentials.access_key}'
+            SECRET_ACCESS_KEY '{credentials.secret_key}'
+            REGION '{self.region}'
+            TIMEFORMAT AS 'epochmillisecs'
+            TRUNCATECOLUMNS BLANKASNULL EMPTYASNULL
+            {self.file_format} 'auto'
+        """)
